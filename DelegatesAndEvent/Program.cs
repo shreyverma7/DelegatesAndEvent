ccf915f1c5ec883b764790ea52d853f8d6e5930a
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-31 11:53:47 +0530

    [Shrey] Add : Folder creation

---
+++ DelegatesAndEvent/Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Provides a good way to encapsulate the methods.\r\nDelegates are the library class in System namespace.\r\nThese are the type-safe pointer of any method.\r\nDelegates are mainly used in implementing the call-back methods and events.\r\nDelegates can be chained together as two or more methods can be called on a single event.\r\nIt doesn’t care about the class of the object that it references.\r\nDelegates can also be used in “anonymous methods” invocation.");
+    }
+}
